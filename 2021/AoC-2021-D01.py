d8cfec9e9c008a39e2c3ea880cf56a0120c88226
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-02 10:29:58 +1000

    D01 solution complete

--- 2021/AoC-2021-D01.py
+++ 2021/AoC-2021-D01.py
@@ -1,12 +1,31 @@
 # AoC 2021 D1
 
-# AoC 2021 D1
+def read_data(filename):
+	with open(filename) as file:
+		lines = list(map(int, file.readlines()))
+	file.close()
+	return lines
 
-with open('/Users/mummydaddy/OneDrive/Documents/GitHub/D01-input.txt') as f:
-	lines = f.readlines()
-	f.close()
 
-for line in lines:
-	if line < line.next():
-		counter += 1
-print(counter)
+def part1(lines):
+	counter = 0
+	for index, line in enumerate(lines):
+		if index + 1 == len(lines):
+			break
+		if line < lines[index+1]:
+			counter += 1
+	return counter
+
+def part2(lines):
+	counter = 0
+	for index, line in enumerate(lines):
+		if index + 3 == len(lines):
+			break
+		if line + lines[index+1] + lines[index+2] < lines[index+1] + lines[index+2] + lines[index+3]:
+			counter += 1
+	return counter
+
+
+input_data = read_data('/home/christopher/Documents/GitHub/adventofcode/2021/D01-input.txt')
+print("Answer part 1: ", part1(input_data))
+print("Answer part 2: ", part2(input_data))
