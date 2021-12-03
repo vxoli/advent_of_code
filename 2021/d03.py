6ff35dd96f26f3ff0126935a5c59f39ec6e7e378
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-03 17:54:16 +1000

    d03 part 1&2 complete

--- 2021/d03.py
+++ 2021/d03.py
@@ -1,4 +1,4 @@
-# AoC 2021 D03
+# oxygen_ratingoC 2021 D03
 
 def read_data(filename):
 	with open(filename) as file:
@@ -17,39 +17,38 @@ def part1(report):
 	for line in report:
 		for number in range(0,len(line)-1):
 			frequency[number] += int(line[number])
-		gamma[0] = int(frequency[0] > 500)
-		epsilon[0] = int(not(gamma[0]))
-		gamma[1] = int(frequency[1] > 500)
-		epsilon[1] = int(not(gamma[1]))
-		gamma[2] = int(frequency[2] > 500)
-		epsilon[2] = int(not(gamma[2]))
-		gamma[3] = int(frequency[3] > 500)
-		epsilon[3] = int(not(gamma[3]))
-		gamma[4] = int(frequency[4] > 500)
-		epsilon[4] = int(not(gamma[4]))
-		gamma[5] = int(frequency[5] > 500)
-		epsilon[5] = int(not(gamma[5]))
-		gamma[6] = int(frequency[6] > 500)
-		epsilon[6] = int(not(gamma[6]))
-		gamma[7] = int(frequency[7] > 500)
-		epsilon[7] = int(not(gamma[7]))
-		gamma[8] = int(frequency[8] > 500)
-		epsilon[8] = int(not(gamma[8]))
-		gamma[9] = int(frequency[9] > 500)
-		epsilon[9] = int(not(gamma[9]))
-		gamma[10] = int(frequency[10] > 500)
-		epsilon[10] = int(not(gamma[10]))
-		gamma[11] = int(frequency[11] > 500)
-		epsilon[11] = int(not(gamma[11]))
-	
-		gamma_value = gamma[0]*2**11 + gamma[1]*2**10 + gamma[2]*2**9 + gamma[3]*2**8 + gamma[4]*2**7 + gamma[5] *2**6 + gamma[6]*2**5 + gamma[7]*2**4 + gamma[8]*2**3 + gamma[9]*2**2 + gamma[10]*2**1 + gamma[11]*2**0
-		epislon_value = epsilon[0]*2**11 + epsilon[1]*2**10 + epsilon[2]*2**9 + epsilon[3]*2**8 + epsilon[4]*2**7 + epsilon[5] *2**6 + epsilon[6]*2**5 + epsilon[7]*2**4 + epsilon[8]*2**3 + epsilon[9]*2**2 + epsilon[10]*2**1 + epsilon[11]*2**0
+			gamma[number] = int(frequency[number] > 500)
+			epsilon[number] = int(not(gamma[number]))
 
-	print(gamma, "--", gamma_value)
-	print(epsilon, "--", epislon_value)
+	gamma_value = gamma[0]*2**11 + gamma[1]*2**10 + gamma[2]*2**9 + gamma[3]*2**8 + gamma[4]*2**7 + gamma[5] *2**6 + gamma[6]*2**5 + gamma[7]*2**4 + gamma[8]*2**3 + gamma[9]*2**2 + gamma[10]*2**1 + gamma[11]*2**0
+	epislon_value = epsilon[0]*2**11 + epsilon[1]*2**10 + epsilon[2]*2**9 + epsilon[3]*2**8 + epsilon[4]*2**7 + epsilon[5] *2**6 + epsilon[6]*2**5 + epsilon[7]*2**4 + epsilon[8]*2**3 + epsilon[9]*2**2 + epsilon[10]*2**1 + epsilon[11]*2**0
 
 	return gamma_value * epislon_value
+
+def part2(report):
+	oxygen_rating = list(report)
+	scrubber_rating = list(report)
+	for i in range(len(report[0])):
+		if len(oxygen_rating) > 1:
+			oxygen_rating0 = len([x for x in oxygen_rating if x[i] == '0'])
+			oxygen_rating1 = len([x for x in oxygen_rating if x[i] == '1'])
+			if oxygen_rating1 >= oxygen_rating0:
+				oxygen_rating = [x for x in oxygen_rating if x[i] == '1']
+			else:
+				oxygen_rating = [x for x in oxygen_rating if x[i] == '0']
+		if len(scrubber_rating) > 1:
+			scrubber_rating0 = len([x for x in scrubber_rating if x[i] == '0'])
+			scrubber_rating1 = len([x for x in scrubber_rating if x[i] == '1'])
+			if scrubber_rating1 >= scrubber_rating0:
+				scrubber_rating = [x for x in scrubber_rating if x[i] == '0']
+			else:
+				scrubber_rating = [x for x in scrubber_rating if x[i] == '1']
+
+	return int(oxygen_rating[0],2) * int(scrubber_rating[0],2)
+
+
 
 
 input_data = read_data('/home/christopher/Documents/GitHub/adventofcode/2021/d03-input.txt')
 print("Answer Part 1: ", part1(input_data))
+print("Answer Part 2: ", part2(input_data))
