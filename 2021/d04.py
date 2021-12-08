18e0be0613a5a5ed6aae18372b20b9cf43388413
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-09 09:17:31 +1000

    d04 got seperating boards right

--- 2021/d04.py
+++ 2021/d04.py
@@ -4,8 +4,8 @@ def read_data(filename):
 	with open(filename) as file:
 		data = [line.strip('\n') for line in list(file)]
 	file.close()
-	#strip the newlines '\n'
-	data = [line.strip('') for line in data]
+	# #strip the newlines '\n'
+	# data = [line.strip('') for line in data]
 
 	return data
 
@@ -14,21 +14,26 @@ def split_data(data): # split given data into game numbers and boards
 	board_numbers = data[2:]
 
 	#split the boards into array of n boards
-	boards = [[0] *5] * 100
+	board = [[0] * 7 for i in range(100)]
 	board_counter = 0
 
 	for index in range(len(board_numbers)):
 		if board_numbers[index] == '': 
 			board_counter += 1
 			continue
-		boards[board_counter][index%6] = board_numbers[index]
+	
 		print(index, "---",index % 6, "---", board_counter, board_numbers[index])
+		# board[0] = board number
+		# board[1] = true/ false - has the board been solved
+		# board[2-7] = lists of 5 numbers for each row
+		board[board_counter][0] = board_counter
+		board[board_counter][2 + index % 6] = board_numbers[index]
 
-	return (game_numbers, boards)
+
+	return (game_numbers, board)
 
 input_data = read_data('/home/christopher/Documents/GitHub/adventofcode/2021/d04-input.txt')
 print(input_data)
 #split the given data in to game numbers drawn and boards
-numbers, boards = split_data(input_data)
-print(numbers)
-print(boards[0][0:])
+(numbers, boards) = split_data(input_data)
+print(boards[99])
