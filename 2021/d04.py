697bb02944449920d601701f85d4736a11d63c70
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-09 09:46:26 +1000

    d04 tidied up splitting of data - need to start working on playing bingo'

--- 2021/d04.py
+++ 2021/d04.py
@@ -22,18 +22,26 @@ def split_data(data): # split given data into game numbers and boards
 			board_counter += 1
 			continue
 	
-		print(index, "---",index % 6, "---", board_counter, board_numbers[index])
 		# board[0] = board number
 		# board[1] = true/ false - has the board been solved
 		# board[2-7] = lists of 5 numbers for each row
 		board[board_counter][0] = board_counter
-		board[board_counter][2 + index % 6] = board_numbers[index]
-
+		# strip out spaces and double spaces (for single digit numbers) by converting ' ' and '  ' to '!' then strip '!'
+		board[board_counter][2 + index % 6] = list(board_numbers[index].replace('  ', '!').replace(' ','!').split('!'))	
 
 	return (game_numbers, board)
 
+def play_bingo():
+# an idea to play
+# if a number on board - that number knocked out or converted to 999 say
+# if an elemenet of list is all 999 - a row copleted
+# a nested for loop can check if columns all 999
+# if a solution found flag with board[1] set to true/ 1
+
+	return
+
 input_data = read_data('/home/christopher/Documents/GitHub/adventofcode/2021/d04-input.txt')
-print(input_data)
 #split the given data in to game numbers drawn and boards
 (numbers, boards) = split_data(input_data)
-print(boards[99])
+# play bingo
+play_bingo()
