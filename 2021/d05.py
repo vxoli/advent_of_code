38bd9a35e101981a230e22175c5d2b8667d7029c
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-11 03:02:34 +1000

    d05 part 1 solved

--- 2021/d05.py
+++ 2021/d05.py
@@ -26,12 +26,31 @@ def part1(coordinates):
 		max_y = (max_y < y1) * y1 + (max_y >= y1) * max_y
 		max_y = (max_y < y2) * y2 + (max_y >= y2) * max_y
 
-	map = [[0]* max_x for i in range(max_y) ]
+	map = [[0]* (((max_x>=max_y)*max_x + (max_x < max_y)*max_y)+1)  for i in range( (((max_x>=max_y)*max_x + (max_x < max_y)*max_y))+1 )]
 
 # 2. loop through coordinates and if x1 == x2 or y1 == y2 then plot on map by incrementing each location
+	for line in coordinates:
+		x1 = int(line.split(' -> ')[0].split(',')[0])
+		y1 = int(line.split(' -> ')[0].split(',')[1])
+		x2 = int(line.split(' -> ')[1].split(',')[0])
+		y2 = int(line.split(' -> ')[1].split(',')[1])
+		if x1 == x2: # horizontal line
+			if y1 > y2: y1,y2 = y2,y1
+			for y in range(y1,y2+1):
+				map[x1][y] += 1
+		elif y1 == y2: # vertical line
+			if x1 > x2: x1,x2 = x2,x1
+			for x in range(x1,x2+1):
+				map[x][y1] += 1
 
-	return
+# 3. count number locations greater than 1
+	counter = 0
+	total = 0
+	for row in map:
+		counter = sum(i>=2 for i in row)
+		total += counter
+
+	return total
 
 input_data = read_data('/home/christopher/Documents/GitHub/adventofcode/2021/d05-input.txt')
-print(input_data[1].split(' -> '))
-part1(input_data)
+print("Part 1: At how many points do at least two lines overlap? ", part1(input_data))
