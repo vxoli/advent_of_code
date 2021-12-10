d02443c4df43893e2398a7854b696b2c6b596993
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-10 20:22:01 +1000

    d05 part1 start

--- 2021/d05.py
+++ 2021/d05.py
@@ -7,12 +7,28 @@ def read_data(filename):
 	return data
 
 def part1(coordinates):
+
+	# 1. need to determine size of grid and define 2D array [max-x,max-y] to form map. set all to 0
+	# 2. loop through coordinates and if x1 == x2 or y1 == y2 then plot on map by incrementing each location
+	# 3. count number locations greater than 1
+
+	# 1. need to determine size of grid and define 2D array [max-x,max-y] to form map. set all to 0
+	max_x = 0
+	max_y = 0
 	for line in coordinates:
 		x1 = int(line.split(' -> ')[0].split(',')[0])
 		y1 = int(line.split(' -> ')[0].split(',')[1])
 		x2 = int(line.split(' -> ')[1].split(',')[0])
 		y2 = int(line.split(' -> ')[1].split(',')[1])
+		
+		max_x = (max_x < x1) * x1 + (max_x >= x1) * max_x
+		max_x = (max_x < x2) * x2 + (max_x >= x2) * max_x
+		max_y = (max_y < y1) * y1 + (max_y >= y1) * max_y
+		max_y = (max_y < y2) * y2 + (max_y >= y2) * max_y
 
+	map = [[0]* max_x for i in range(max_y) ]
+
+# 2. loop through coordinates and if x1 == x2 or y1 == y2 then plot on map by incrementing each location
 
 	return
 
