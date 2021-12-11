6e4000f508bff9818c1edd35b4270513625dfff8
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-11 19:28:32 +1000

    d07 part 1 & part 2 complete

--- 2021/d07.py
+++ 2021/d07.py
@@ -8,11 +8,7 @@ def read_data(filename):
 	return data
 
 def part1(position_data):
-	#try this:
-	# determine mode
-	# deterine differences from position to mode
-	# sum the differences to find amount of fuel
-
+	# median is value with least total differences
 	median_position = round(median(position_data),0)
 	fuel_sum = 0
 	difference = 0
@@ -21,7 +17,16 @@ def part1(position_data):
 		fuel_sum += difference
 	return fuel_sum
 
+def part2(position_data):
+	#now mean is value with least total differences
+
+	mean_position = round(mean(position_data),0)
+	fuel_sum = 0
+	difference = 0
+	maxl=max([abs(int(i)) for i in  position_data])
+	return int(min([sum([(abs(int(i)-j)+1)*(abs(int(i)-j))/2 for i in  position_data]) for j in range(maxl)]))
+
 # MAIN
 input_data = read_data('/home/christopher/Documents/GitHub/adventofcode/2021/d07-input.txt')
 print("Part 1: How much fuel must they spend to align to that position? ", part1(input_data))
-
+print("Part 2: How much fuel must they spend to align to that position? ", part2(input_data))
