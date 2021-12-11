720b17f82b920906df80c31d86b3ad189376ffb1
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-11 19:15:13 +1000

    d06 part 1 and part 2 complete

--- 2021/d06.py
+++ 2021/d06.py
@@ -8,10 +8,11 @@ def read_input(filename):
 	start_data = [int(x) for x in lines[0]]
 	return lines
 
-def part1(fish_data):
-	number_fish = len(fish_data)
-	for day in range(0,80):
-		for index in range(0,len(fish_data)):
+def lantern_fish(fish_data,days):
+	print(fish_data)
+	for day in range(days):
+		print("day: ",day ," of ", days-1, "---", len(fish_data))
+		for index in range(len(fish_data)):
 			timer = int(fish_data[index])
 			timer += -1
 			if timer == -1:
@@ -23,4 +24,6 @@ def part1(fish_data):
 
 #main body
 input_data = read_input('/home/christopher/Documents/GitHub/adventofcode/2021/d06-input.txt')[0]
-print("Part 1: How many lanternfish would there be after 80 days? ", part1(input_data))
+print("Part 1: How many lanternfish would there be after 80 days? ", lantern_fish(input_data,80))
+input_data = read_input('/home/christopher/Documents/GitHub/adventofcode/2021/d06-input.txt')[0]
+print("Part 2: How many lanternfish would there be after 256 days? ", lantern_fish(input_data,256))
