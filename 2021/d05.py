11fcff12d0165dde91523cbb4debae0592abb34c
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-11 15:11:20 +1000

    d05 part1 & part2 complete

--- 2021/d05.py
+++ 2021/d05.py
@@ -77,11 +77,10 @@ def part2(coordinates):
 		x2 = int(line.split(' -> ')[1].split(',')[0])
 		y2 = int(line.split(' -> ')[1].split(',')[1])
 		if x1 == x2: # horizontal line
-			print('HORIZONTAL')
+			if y1 > y2: y1,y2 = y2,y1
 			for y in range(y1,y2+1):
 				map[x1][y] += 1
 		elif y1 == y2: # vertical line
-			print('VERTICAL')
 			if x1 > x2: x1,x2 = x2,x1
 			for x in range(x1,x2+1):
 				map[x][y1] += 1
@@ -89,26 +88,22 @@ def part2(coordinates):
 		else:
 			x = x1
 			y = y1
-			if x1 < x2 and y1 < y2: 
-				print('DIAGONAL top Left to bottom Right')
+			if x1 < x2 and y1 < y2:
 				while x <= x2 and y <= y2:
 					map[x][y] += 1
 					x += 1
 					y += 1
 			if x1 < x2 and y1 > y2:
-				print('DIAGONAL top right to bottom left')
 				while x <= x2 and y >= y2:
 					map[x][y] += 1
 					x += 1
 					y += -1
 			if x1 > x2 and y1 < y2:
-				print('DIAGONAL bottom left to top right')
 				while x >= x2 and y <= y2:
 					map[x][y] += 1
 					x += -1
 					y += 1
 			if x1 > x2 and y1 > y2: 
-				print('DIAGONAL bottom right to top left')
 				while x >= x2 and y >= y2:
 					map[x][y] += 1
 					x += -1
@@ -126,4 +121,4 @@ def part2(coordinates):
 # start main block
 input_data = read_data('/home/christopher/Documents/GitHub/adventofcode/2021/d05-input.txt')
 print("Part 1: At how many points do at least two lines overlap? ", part1(input_data))
-print(part2(input_data))
+print("Part 2: Consider all of the lines. At how many points do at least two lines overlap? ", part2(input_data))
