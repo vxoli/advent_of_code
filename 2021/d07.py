3af8fe825e7b5b716db2da26d65b8a0655e4d557
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-14 09:51:47 +1000

    d07 modified

--- 2021/d07.py
+++ 2021/d07.py
@@ -10,12 +10,17 @@ def read_data(filename):
 def part1(position_data):
 	# median is value with least total differences
 	median_position = round(median(position_data),0)
-	fuel_sum = 0
-	difference = 0
+	mean_position = round(mean(position_data),0)
+	fuel_sum_part1 = 0
+	fuel_sum_part2 = 0
+	difference_part1 = 0
+	difference_part2 = 0
 	for position in position_data:
-		difference = abs(position - median_position)
-		fuel_sum += difference
-	return fuel_sum
+		difference_part1 = abs(position - median_position)
+		difference_part2 = abs(position - mean_position)
+		fuel_sum_part1 += difference_part1
+		fuel_sum_part2 += (difference_part2 * (difference_part2+1))/2 #(1+2+3+..+n = n(n+1)/2)
+	return fuel_sum_part1, fuel_sum_part2
 
 def part2(position_data):
 	#now mean is value with least total differences
@@ -28,5 +33,5 @@ def part2(position_data):
 
 # MAIN
 input_data = read_data('/home/christopher/Documents/GitHub/adventofcode/2021/d07-input.txt')
-print("Part 1: How much fuel must they spend to align to that position? ", part1(input_data))
-print("Part 2: How much fuel must they spend to align to that position? ", part2(input_data))
+print("Part 1: How much fuel must they spend to align to that position? ", part1(input_data)[0])
+print("Part 2: How much fuel must they spend to align to that position? ", part1(input_data)[1])
