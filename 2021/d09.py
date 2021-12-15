b1d4b9f7f8a6544e9395458837fd3242214be621
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-15 12:50:18 +1000

    d09 part 2 completed

--- 2021/d09.py
+++ 2021/d09.py
@@ -35,12 +35,14 @@ def part1(heightmap):
 	return risklevel
 
 def part2(heightmap):
-	rows = len(heightmap)
+	intmap = [[int(x) for x in list(line)] for line in heightmap]
+	rows = len(heightmap)-1
 	cols = len(heightmap[0])
 
 	low = []
 	cur_id = 1
-	ids = np.zeros((rows, cols), dtype=int)
+#	ids = np.zeros((rows, cols), dtype=int)
+	ids = [[0 for col in range(cols)] for row in range(rows)]
 
 	# Find low points
 	for row in range(rows):
@@ -53,7 +55,7 @@ def part2(heightmap):
 	            if not ((0 <= rr and rr < rows) and (0 <= cc and cc < cols)):
 	                continue
 
-	            if heightmap[rr][cc] <= heightmap[row][col]:
+	            if intmap[rr][cc] <= intmap[row][col]:
 	                is_low = False
 	                break
 
@@ -71,7 +73,7 @@ def part2(heightmap):
 	            continue
 	        visited.add((row, col))
 
-	        ids[row, col] = cur_id
+	        ids[row][col] = cur_id
 
 	        for d in [(0, 1), (0, -1), (-1, 0), (1, 0)]:
 	            rr = row + d[0]
@@ -80,7 +82,7 @@ def part2(heightmap):
 	            if not ((0 <= rr and rr < rows) and (0 <= cc and cc < cols)):
 	                continue
 
-	            if heightmap[rr][cc] == 9:
+	            if intmap[rr][cc] == 9:
 	                continue
 
 	            stack.append((rr, cc))
@@ -90,12 +92,12 @@ def part2(heightmap):
 	# Find the sizes of biggest basins
 	sizes = [0] * cur_id
 
-	for x in ids.flatten():
-	    sizes[x] += 1
+	for x in [item for sublist in ids for item in sublist]:
+		sizes[x] += 1
 	sizes = sizes[1:]
 
 	sizes.sort()
-	print(sizes[-1] * sizes[-2] * sizes[-3])
+	return (sizes[-1] * sizes[-2] * sizes[-3])
 
 
 
@@ -103,5 +105,5 @@ def part2(heightmap):
 
 # main
 input_data = read_data('/home/christopher/Documents/GitHub/adventofcode/2021/d09-input.txt')
-print(part1(input_data))
-#part2(input_data)
+print("Part 1: What is the sum of the risk levels of all low points on your heightmap? ",part1(input_data))
+print("Part 2: What do you get if you multiply together the sizes of the three largest basins? ",part2(input_data))
