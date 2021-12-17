7c136253746aefcd53502669abc5bd92eb52c4d1
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-17 11:53:06 +1000

    d13 start

--- 2021/d13.py
+++ 2021/d13.py
@@ -1,3 +1,18 @@
 # AoC 2021 d13
 # --- Day 13: Transparent Origami ---
 
+import urllib.request
+
+def read_url():
+	file = urllib.request.urlopen('https://raw.githubusercontent.com/vxoli/adventofcode/main/2021/d13-input.txt')
+	data = file.read().strip()
+	data = data.decode("utf8")
+	data = data.split("\n")
+	folds = [x for x in data if x[:4] == "fold"]
+	grid = data[:len(folds)]
+
+	return grid,folds
+
+# MAIN
+grid_data, fold_data = read_url()
+print(fold_data)
