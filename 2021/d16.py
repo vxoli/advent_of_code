706e826c1547ca941e9c61d5102034e5382c1a12
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-20 11:38:02 +1000

    d16 part 1 start - convert hex to binary working

--- 2021/d16.py
+++ 2021/d16.py
@@ -8,7 +8,17 @@ def read_url(url):
 	data = file.read().strip()
 	data = data.decode("utf8")
 	data = data.split("\n")
-	return
+	# Test Data
+	data[0]= 'D2FE28'
+	return data[0]
+
+def convert_hex_to_bin(hex_string):
+	binary = ""
+	for char in hex_string:
+		binary += str(bin(int(char, 16))[2:].zfill(4))
+	return binary
 
 # MAIN
-input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2021/d15-input.txt')
+transmission_hex = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2021/d16-input.txt')
+transmission_bin = convert_hex_to_bin(transmission_hex)
+print(transmission_bin)
