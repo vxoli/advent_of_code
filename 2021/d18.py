15bee0c334b54fa1ecfb9f393b623827c5655e79
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-24 13:58:18 +1000

    d18 setup

---
+++ 2021/d18.py
@@ -0,0 +1,17 @@
+# AoC 2021 d18
+# --- Day 18: Snailfish ---
+
+import urllib.request
+
+def read_url(url):
+	file = urllib.request.urlopen(url)
+	data = file.read().strip()
+	data = data.decode("utf8")
+	data = data.split("\n")
+	
+	return data
+
+
+# MAIN
+data = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2021/d18-input.txt')
+print(data)
