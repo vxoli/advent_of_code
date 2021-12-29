52d7feec4e7a6ea83bb148cad7135fe9b1995758
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-29 16:32:47 +1000

    d25 part1 starting solution

--- 2021/d25.py
+++ 2021/d25.py
@@ -1,5 +1,6 @@
-# AoC 2021 d22
-# --- Day 22: Reactor Reboot ---
+# AoC 2021 d25
+# --- Day 25: Sea Cucumber ---
+
 import urllib.request
 
 def read_url(url):
@@ -9,3 +10,39 @@ def read_url(url):
 	data = data.split("\n")
 
 	return data
+
+map = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2021/d25-input.txt')
+
+moved = True
+steps = 0
+while moved:
+	moved=False
+	new_map = [[[]for _ in range(len(map[0]))] for _ in range(len(map))]
+	for i,line in enumerate(map):
+		for j,char in enumerate(line):
+
+			if char == ".": 
+				new_map[i][j] = "."
+				continue
+
+			if char == ">":
+				if map[i][((j+1)%(len(line)))] != ".":
+					new_map[i][j] = ">"
+					continue
+				else:
+					new_map[i][j]="."
+					new_map[i][((j+1)%(len(line)))]=">"
+					moved=True
+					continue
+
+			if char == "v":
+				if map [((i+1)%(len(map)))][j] != ".":
+					new_map[i][j] = "v"
+					continue
+				else:
+					new_map[i][j] = "."
+					new_map[((i+1)%(len(map)))][j] = 'v'
+					moved=True
+					continue
+	steps += 1
+	map = new_map
