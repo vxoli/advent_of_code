1ff8747eaa6e8943b7b8932c00f95e26a04e3656
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-12-30 10:31:27 +1000

    d25 part 1 complete

--- 2021/d25.py
+++ 2021/d25.py
@@ -2,47 +2,48 @@
 # --- Day 25: Sea Cucumber ---
 
 import urllib.request
+import copy
 
 def read_url(url):
 	file = urllib.request.urlopen(url)
 	data = file.read().strip()
 	data = data.decode("utf8")
-	data = data.split("\n")
+#	data = data.split("\n")
+	data = [list(line) for line in data.split("\n")]
 
 	return data
 
+def move(grid, indicator):
+	new_grid = copy.deepcopy(grid)
+	any_moved = False
+	for row in range(rows):
+		for col in range(cols):
+			if indicator != grid[row][col]:
+				continue
+			if grid[row][col] == "v":
+				dest = (row+1)%rows, col
+			else:
+				dest = row,(col+1)%cols
+
+			if grid[dest[0]][dest[1]] == ".":
+				any_moved = True
+				new_grid[dest[0]][dest[1]] = grid[row][col]
+				new_grid[row][col] = "."
+
+	return new_grid, any_moved
+
+# Main
 map = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2021/d25-input.txt')
 
-moved = True
+rows = len(map)
+cols = len(map[0])
+
 steps = 0
-while moved:
-	moved=False
-	new_map = [[[]for _ in range(len(map[0]))] for _ in range(len(map))]
-	for i,line in enumerate(map):
-		for j,char in enumerate(line):
 
-			if char == ".": 
-				new_map[i][j] = "."
-				continue
-
-			if char == ">":
-				if map[i][((j+1)%(len(line)))] != ".":
-					new_map[i][j] = ">"
-					continue
-				else:
-					new_map[i][j]="."
-					new_map[i][((j+1)%(len(line)))]=">"
-					moved=True
-					continue
-
-			if char == "v":
-				if map [((i+1)%(len(map)))][j] != ".":
-					new_map[i][j] = "v"
-					continue
-				else:
-					new_map[i][j] = "."
-					new_map[((i+1)%(len(map)))][j] = 'v'
-					moved=True
-					continue
+while True:
+	map, any_moved_e = move(map, ">")
+	map, any_moved_s = move(map, "v")
 	steps += 1
-	map = new_map
+	if not(any_moved_e or any_moved_s): break
+
+print("Part 1: What is the first step on which no sea cucumbers move?: ",steps) 
