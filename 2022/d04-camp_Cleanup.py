3c53818fc019f67fc342895d1df109b30672f8e3
Christopher STONELL <vxoli-dfoxb@yahaoo.co.uk>
2022-12-04 16:13:15 +1000

    d04 setup

--- 2022/d04-camp_Cleanup.py
+++ 2022/d04-camp_Cleanup.py
@@ -11,4 +11,32 @@ def read_url(url):
 	
 	return data
 
+def part_1(data):
+    counter = 0 
+    # In how many assignment pairs does one range fully contain the other?
+    for pair in data:
+        elf1_assigned = ""
+        elf2_assigned = ""
+        elf1,elf2 = pair.split(',')
+        elf1_start,elf1_end = elf1.split('-')
+        elf2_start,elf2_end = elf2.split('-')
+
+        #generate list of assigned area IDs
+        for i in range(int(elf1_start),int(elf1_end)+1):
+            elf1_assigned += str(i)+'-'
+        for i in range(int(elf2_start), int(elf2_end)+1):
+            elf2_assigned += str(i)+'-'
+        elf1_assigned, elf2_assigned = elf1_assigned[:-1], elf2_assigned[:-1]
+        print(elf1,"   ",elf2)
+        print(elf1_assigned,"\n",elf2_assigned)
+        print((elf1_assigned in elf2_assigned) or (elf2_assigned in elf1_assigned))
+
+        # check if one assigned list contained within the other
+        #if (elf1_assigned in elf2_assigned) or (elf2_assigned in elf1_assigned):
+        #    counter += 1
+        counter += (elf1_assigned in elf2_assigned) + (elf2_assigned in elf1_assigned)
+
+    return counter
+
 data = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d04-input.txt')
+print(part_1(data))
