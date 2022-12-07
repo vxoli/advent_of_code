d2e2a7a0ffa84495f01ade292be58c4289dd903e
Christopher STONELL <vxoli-dfoxb@yahaoo.co.uk>
2022-12-07 18:51:19 +1000

    d07 part 1 complete

--- 2022/d07_No_Space_Left.py
+++ 2022/d07_No_Space_Left.py
@@ -2,6 +2,8 @@
 # --- Day 07 No Space Left on Device ---
 
 import urllib.request
+from collections import defaultdict
+from itertools import accumulate
 
 def read_url(url):
 	file = urllib.request.urlopen(url)
@@ -12,9 +14,23 @@ def read_url(url):
 	return data
 
 def part_1(data):
-    print(data)
-    return
+	sizes = defaultdict(int)
+	stack = []
+	for line in data:
+		if line.startswith('$ ls') or line.startswith('dir'):
+			continue
+		
+		match line.split():
+			case "$", 'cd', '..':
+				stack.pop()
+			case '$', 'cd', directory:
+				stack.append(directory)
+			case size, _:
+				for path in accumulate(stack, func=lambda a,b: a+"/"+b):
+					sizes[path] += int(size)
+			
+	return (sum(size for size in sizes.values() if size <= 100000))
 
 
 data = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d07-input.txt')
-part_1(data)
+print(part_1(data))
