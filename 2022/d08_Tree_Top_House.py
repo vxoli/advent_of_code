3b97527647fb21641b44df4c6e0ef9ba58f1408a
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2022-12-08 15:06:19 +1000

    d08 setup

---
+++ 2022/d08_Tree_Top_House.py
@@ -0,0 +1,14 @@
+# AoC 2022
+# --- Day 08 Tree Top House ---
+
+import urllib.request
+
+def read_url(url):
+	file = urllib.request.urlopen(url)
+	data = file.read().strip()
+	data = data.decode("utf8")
+	data = data.split("\n")
+	
+	return data
+
+data = read_url('')
