1852a2cd3e81b0ab4fcfea8144a28aa594b00998
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2022-12-09 18:43:49 +1000

    d09 setup

---
+++ 2022/d09_Rope_Bridge.py
@@ -0,0 +1,14 @@
+# AoC 2022
+# --- Day 09 Rope Bridge ---
+
+import urllib.request
+
+def read_url(url):
+	file = urllib.request.urlopen(url)
+	data = file.read().strip()
+	data = data.decode("utf8")
+	data = data.split("\n")
+	
+	return data
+
+print(read_url(''))
