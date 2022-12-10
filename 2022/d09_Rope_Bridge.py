a2c1ed65415ea6cd8cb2ce2db6c430b5849d2c04
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2022-12-10 10:19:39 +1000

    d09 part 1 complete

--- 2022/d09_Rope_Bridge.py
+++ 2022/d09_Rope_Bridge.py
@@ -24,10 +24,8 @@ def part_1(commands):
         for step in range(steps):
             headx += delta[direction][0]
             heady += delta[direction][1]
-            print('H:',headx, '===', heady)
             tail_dist_x = headx-tailx
             tail_dist_y = heady-taily
-            print(tail_dist_x, '---', tail_dist_y)
             if abs(tail_dist_x) > 1 or abs(tail_dist_y) > 1: #tail has to move to catch up
                 # moves for left right up down first
                 if tail_dist_x == 2 and tail_dist_y == 0: #too far right - tail move right to catch up
@@ -55,20 +53,13 @@ def part_1(commands):
                 elif (tail_dist_x == 1 and tail_dist_y == -2) or (tail_dist_x == 2 and tail_dist_y == -1):
                     tailx += 1
                     taily -= 1
-                print('T:',tailx,',',taily)
-                print(tail_posn_list)
-                if [tailx,taily] not in tail_posn_list: tail_posn_list.append([tailx,taily])
-                print(tailx,',',taily)
-                print(tail_posn_list)
- 
-    # track tail positions in set of tuples/ lists - then count items in resulting set/list
 
-    print(headx,',',heady, '---', tailx,',',taily)
-    print(len(tail_posn_list)+1)
-    return
+                if [tailx,taily] not in tail_posn_list: tail_posn_list.append([tailx,taily])
+ 
+    return len(tail_posn_list)+1
 
 
 input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d09-input.txt')
 commands = [line.split() for line in input]
-commands = [['R', 4], ['U', 4] , ['L', 3] , ['D', 1], ['R', 4], ['D', 1], ['L', 5], ['R', 2]]
-print(part_1(commands))
+#commands = [['R', 4], ['U', 4] , ['L', 3] , ['D', 1], ['R', 4], ['D', 1], ['L', 5], ['R', 2]] # test data
+print("Part 1: Simulate your complete hypothetical series of motions. How many positions does the tail of the rope visit at least once?",part_1(commands))
