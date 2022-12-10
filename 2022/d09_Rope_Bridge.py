c521fcf12c7de6bad08a3034d652eb31e157948d
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2022-12-10 12:17:18 +1000

    d09 if-elif decision block changed to match-case

--- 2022/d09_Rope_Bridge.py
+++ 2022/d09_Rope_Bridge.py
@@ -13,8 +13,8 @@ def read_url(url):
 
 def part_1(commands):
     start = [1,1] # (x,y)
-    headx, heady = 1,1
-    tailx, taily = 1,1
+    head_x, head_y = 1,1
+    tail_x, tail_y = 1,1
     tail_posn_list = []
     delta = {'R': (1,0), 'L': (-1,0), 'U':(0,1), 'D':(0,-1)}
     for line in commands:
@@ -22,84 +22,58 @@ def part_1(commands):
         steps = int(line[1])
 
         for step in range(steps):
-            headx += delta[direction][0]
-            heady += delta[direction][1]
-            tail_dist_x = headx-tailx
-            tail_dist_y = heady-taily
+            head_x += delta[direction][0]
+            head_y += delta[direction][1]
+            tail_dist_x = head_x-tail_x
+            tail_dist_y = head_y-tail_y
             if abs(tail_dist_x) > 1 or abs(tail_dist_y) > 1: #tail has to move to catch up
                 match (tail_dist_x,tail_dist_y):
                     # moves for left right up down first
                     case (2,0):
-                        tailx += 1
+                        tail_x += 1
                     case (0,2):
-                        taily += 1
+                        tail_y += 1
                     case (-2,0):
-                        tailx -= 1
+                        tail_x -= 1
                     case (0,-2):
-                        taily -= 1
+                        tail_y -= 1
                     # moves for diagonal up right, up left, down left and down right - each has two head positions, but tail move same
                     #up right
                     case (1,2):
-                        tailx += 1
-                        taily += 1
+                        tail_x += 1
+                        tail_y += 1
                     case (2,1):
-                        tailx += 1
-                        taily += 1
+                        tail_x += 1
+                        tail_y += 1
                     # up left
                     case (-1,2):
-                        tailx -= 1
-                        taily += 1
+                        tail_x -= 1
+                        tail_y += 1
                     case (-2,1):
-                        tailx -= 1
-                        taily += 1
+                        tail_x -= 1
+                        tail_y += 1
                     # down left
                     case (-1,-2):
-                        tailx -= 1
-                        taily -= 1
+                        tail_x -= 1
+                        tail_y -= 1
                     case (-2,-1):
-                        tailx -= 1
-                        taily -= 1
+                        tail_x -= 1
+                        tail_y -= 1
                     # down right
                     case (1,-2):
-                        tailx += 1
-                        taily -= 1
+                        tail_x += 1
+                        tail_y -= 1
                     case (2,-1):
-                        tailx += 1
-                        taily -= 1
+                        tail_x += 1
+                        tail_y -= 1
 
-                # # moves for left right up down first
-                # if tail_dist_x == 2 and tail_dist_y == 0: #too far right - tail move right to catch up
-                #     tailx += 1
-                # elif tail_dist_x == 0 and tail_dist_y == 2 : # too far up - tail has to move up                    
-                #     taily += 1
-                # elif tail_dist_x == -2 and tail_dist_y == 0: # too far left - tail moves left
-                #     tailx -= 1
-                # elif tail_dist_x == 0 and tail_dist_y == -2: # too far down - tail moves down
-                #     taily -= 1
-                # # moves for diagonal up right, up left, down left and down right - each has two head positions, but tail move same
-                # #up right
-                # elif (tail_dist_x == 1 and tail_dist_y == 2) or (tail_dist_x == 2 and tail_dist_y == 1):
-                #     tailx += 1
-                #     taily += 1
-                # # up left
-                # elif (tail_dist_x == -1 and tail_dist_y == 2) or (tail_dist_x == -2 and tail_dist_y == 1):
-                #     tailx -= 1
-                #     taily += 1
-                # # down left
-                # elif (tail_dist_x == -1 and tail_dist_y == -2) or (tail_dist_x == -2 and tail_dist_y == -1):
-                #     tailx -= 1
-                #     taily -= 1
-                # # down right
-                # elif (tail_dist_x == 1 and tail_dist_y == -2) or (tail_dist_x == 2 and tail_dist_y == -1):
-                #     tailx += 1
-                    # taily -= 1
 
-                if [tailx,taily] not in tail_posn_list: tail_posn_list.append([tailx,taily])
+                if [tail_x,tail_y] not in tail_posn_list: tail_posn_list.append([tail_x,tail_y])
  
     return len(tail_posn_list)+1
 
 
 input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d09-input.txt')
 commands = [line.split() for line in input]
-#commands = [['R', 4], ['U', 4] , ['L', 3] , ['D', 1], ['R', 4], ['D', 1], ['L', 5], ['R', 2]] # test data
+# commands = [['R', 4], ['U', 4] , ['L', 3] , ['D', 1], ['R', 4], ['D', 1], ['L', 5], ['R', 2]] # test data
 print("Part 1: Simulate your complete hypothetical series of motions. How many positions does the tail of the rope visit at least once?",part_1(commands))
