bf18fba401f03e2b0867a50348c527ef0c573c1c
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2022-12-10 10:15:41 +1000

    d09 part 1 seems to work on etst data

--- 2022/d09_Rope_Bridge.py
+++ 2022/d09_Rope_Bridge.py
@@ -13,25 +13,58 @@ def read_url(url):
 
 def part_1(commands):
     start = [1,1] # (x,y)
-    head = [1,1]
-    tail = [1,1]
+    headx, heady = 1,1
+    tailx, taily = 1,1
+    tail_posn_list = []
     delta = {'R': (1,0), 'L': (-1,0), 'U':(0,1), 'D':(0,-1)}
     for line in commands:
         direction = line[0]
         steps = int(line[1])
 
         for step in range(steps):
-            head[0] = head[0] + delta[direction][0]
-            head[1] = head[1] + delta[direction][1]
-            print(head[0], '===', tail[0])
-            tail_dist_x = head[0]-tail[0]
-            tail_dist_y = head[1]-tail[1]
+            headx += delta[direction][0]
+            heady += delta[direction][1]
+            print('H:',headx, '===', heady)
+            tail_dist_x = headx-tailx
+            tail_dist_y = heady-taily
             print(tail_dist_x, '---', tail_dist_y)
-            if tail_dist_x > 1 or tail_dist_y > 1: #tail has to move to catch up
-                print('tail move')
+            if abs(tail_dist_x) > 1 or abs(tail_dist_y) > 1: #tail has to move to catch up
+                # moves for left right up down first
+                if tail_dist_x == 2 and tail_dist_y == 0: #too far right - tail move right to catch up
+                    tailx += 1
+                elif tail_dist_x == 0 and tail_dist_y == 2 : # too far up - tail has to move up                    
+                    taily += 1
+                elif tail_dist_x == -2 and tail_dist_y == 0: # too far left - tail moves left
+                    tailx -= 1
+                elif tail_dist_x == 0 and tail_dist_y == -2: # too far down - tail moves down
+                    taily -= 1
+                # moves for diagonal up right, up left, down left and down right - each has two head positions, but tail move same
+                #up right
+                elif (tail_dist_x == 1 and tail_dist_y == 2) or (tail_dist_x == 2 and tail_dist_y == 1):
+                    tailx += 1
+                    taily += 1
+                # up left
+                elif (tail_dist_x == -1 and tail_dist_y == 2) or (tail_dist_x == -2 and tail_dist_y == 1):
+                    tailx -= 1
+                    taily += 1
+                # down left
+                elif (tail_dist_x == -1 and tail_dist_y == -2) or (tail_dist_x == -2 and tail_dist_y == -1):
+                    tailx -= 1
+                    taily -= 1
+                # down right
+                elif (tail_dist_x == 1 and tail_dist_y == -2) or (tail_dist_x == 2 and tail_dist_y == -1):
+                    tailx += 1
+                    taily -= 1
+                print('T:',tailx,',',taily)
+                print(tail_posn_list)
+                if [tailx,taily] not in tail_posn_list: tail_posn_list.append([tailx,taily])
+                print(tailx,',',taily)
+                print(tail_posn_list)
+ 
+    # track tail positions in set of tuples/ lists - then count items in resulting set/list
 
-
-    print(head)
+    print(headx,',',heady, '---', tailx,',',taily)
+    print(len(tail_posn_list)+1)
     return
 
 
