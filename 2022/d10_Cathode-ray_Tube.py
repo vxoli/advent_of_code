e2dac35c05f56f43fe4ba54a7778942d815dbf85
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2022-12-14 11:07:42 +1000

    d10 code cleaned up a bit

--- 2022/d10_Cathode-ray_Tube.py
+++ 2022/d10_Cathode-ray_Tube.py
@@ -22,17 +22,14 @@ def part_1(instructions):
 				cycle += 1
 				if cycle in [20,60,100,140,180,220]:
 					sum_signal_strengths += cycle * x
-					print('X:',x,'cycle:',cycle, sum_signal_strengths)
 			case 'addx':
 				cycle += 1
 				if cycle in [20,60,100,140,180,220]:
 					sum_signal_strengths += cycle * x
-					print('X:',x,'cycle:',cycle, sum_signal_strengths)
 				cycle += 1
 				x += int(line.split(' ')[1])
 				if cycle in [20,60,100,140,180,220]:
 					sum_signal_strengths += cycle * x
-					print('X:',x,'cycle:',cycle, sum_signal_strengths)
 
 		prev_x, prev_cycle = x,cycle
 
@@ -41,4 +38,4 @@ def part_1(instructions):
 data = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d10-input.txt')
 # data = ['addx 15','addx -11','addx 6','addx -3','addx 5','addx -1','addx -8','addx 13','addx 4','noop','addx -1','addx 5','addx -1','addx 5','addx -1','addx 5','addx -1','addx 5','addx -1','addx -35','addx 1','addx 24','addx -19','addx 1','addx 16','addx -11','noop','noop','addx 21','addx -15','noop','noop','addx -3','addx 9','addx 1','addx -3','addx 8','addx 1','addx 5','noop','noop','noop','noop','noop','addx -36','noop','addx 1','addx 7','noop','noop','noop','addx 2','addx 6','noop','noop','noop','noop','noop','addx 1','noop','noop','addx 7','addx 1','noop','addx -13','addx 13','addx 7','noop','addx 1','addx -33','noop','noop','noop','addx 2','noop','noop','noop','addx 8','noop','addx -1','addx 2','addx 1','noop','addx 17','addx -9','addx 1','addx 1','addx -3','addx 11','noop','noop','addx 1','noop','addx 1','noop','noop','addx -13','addx -19','addx 1','addx 3','addx 26','addx -30','addx 12','addx -1','addx 3','addx 1','noop','noop','noop','addx -9','addx 18','addx 1','addx 2','noop','noop','addx 9','noop','noop','noop','addx -1','addx 2','addx -37','addx 1','addx 3','noop','addx 15','addx -21','addx 22','addx -6','addx 1','noop','addx 2','addx 1','noop','addx -10','noop','noop','addx 20','addx 1','addx 2','addx 2','addx -6','addx -11','noop','noop','noop']
 
-print(part_1(data))
+print("Part 1: What is the sum of these six signal strengths?",part_1(data))
