8317cbb4d1ccc62bc281248319466ca1f6c079cb
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2022-12-14 11:38:44 +1000

    d14 setup

---
+++ 2022/d14.py
@@ -0,0 +1,19 @@
+# AoC 2022
+# --- Day 14: ---
+
+import urllib.request
+
+def read_url(url):
+	file = urllib.request.urlopen(url)
+	data = file.read().strip()
+	data = data.decode("utf8")
+	data = data.split("\n")
+	
+	return data
+
+def part_1(data):
+
+	return
+
+input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d14-input.txt')
+print(input)
