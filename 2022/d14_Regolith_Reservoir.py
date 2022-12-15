5fd770b0d5f13befb51ae9a7b60b901cbf165b4b
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2022-12-15 17:08:18 +1000

    d14 part 1 produce grid

--- 2022/d14_Regolith_Reservoir.py
+++ 2022/d14_Regolith_Reservoir.py
@@ -27,8 +27,11 @@ def make_grid(data):
 	min_x = find_min_max(data,0)[0]
 	max_y = find_min_max(data,1)[1]
 	min_y = 0 #find_min_max(data,1)[0]
+	air = '.'
+	rock = "#"
+	source = "+"
 	print(max_x,',',min_x,',',max_y,',',min_y)
-	grid = [['.' for y in range(max_y-min_y+1)] for x in range(max_y-min_y+1)]
+	grid = [[air for y in range(max_y-min_y+1)] for x in range(max_y-min_y+1)]
 	for line in data:
 		co_ords = line.split('->')
 		for i, pair in enumerate(co_ords[:-1]):
@@ -38,24 +41,22 @@ def make_grid(data):
 			end_y = int(co_ords[i+1].split(',')[1])
 			if start_x == end_x:
 				for y in range(min(start_y,end_y),max(start_y,end_y)+1):
-					grid[y-min_y][start_x-min_x] ='#'
+					grid[y-min_y][start_x-min_x] = rock
 					
 			if start_y == end_y: 
 				for x in range(min(start_x,end_x),max(start_x, end_x)+1):
-					grid[start_y-min_y][x-min_x] ='#'
+					grid[start_y-min_y][x-min_x] = rock
 
-
-		# for x in co_ords:
-		# 	print(x)
-
-
+	grid[0-min_y][500-min_x]=source
 	return grid
 
 def part_1(data):
+	grid = make_grid(data)
+	sand_drip_from = (0,500)
 
-	return
+	return grid
 
 # input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d14-input.txt')
 input = ['498,4 -> 498,6 -> 496,6','503,4 -> 502,4 -> 502,9 -> 494,9']
-grid = make_grid(input)
+grid = part_1(input)
 for line in grid: print(line)
