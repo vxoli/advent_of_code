0ebc990c9a925dd8e90e95793ede3850a286366f
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2022-12-16 16:12:09 +1000

    d16 setup

---
+++ 2022/d16_Proboscidea_Volcanium.py
@@ -0,0 +1,15 @@
+# AoC 2022
+# --- Day 16: Proboscidea Volcanium ---
+
+import urllib.request
+
+def read_url(url):
+	file = urllib.request.urlopen(url)
+	data = file.read().strip()
+	data = data.decode("utf8")
+	data = data.split("\n")
+	
+	return data
+
+
+input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d16-input.txt')
