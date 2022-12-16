95de580911f20494e77c3decf124bf275e1f2fce
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2022-12-16 17:20:13 +1000

    d15 part one - starting new idea - record only positions that cross given level

--- 2022/d15_Beacon_Exclusion_Zone.py
+++ 2022/d15_Beacon_Exclusion_Zone.py
@@ -38,21 +38,36 @@ def parse(input):
 	min_b_x = min([line[1][0] for line in co_ordinates])
 	min_b_y = min([line[1][1] for line in co_ordinates])
 	offsetx, offsety = min(min_s_x,min_b_x), min(min_s_y,min_b_y)
-	grid = [['.' for x in range(max(max_b_x, max_s_x)-min(min_b_x,min_s_x)-offsetx+1)] for y in range(max(max_s_y, max_b_y)-min(min_s_y,min_b_y)-offsety+1)]
 	
-	for line in co_ordinates:
-		x = line[0][0]
-		y = line[0][1]
-		grid[y-offsety][x-offsetx] = SENSOR
-		x = line[1][0]
-		y = line[1][1]
-		grid[y-offsety][x-offsetx] = BEACON
-	for line in grid: print(''.join(line))
+	return co_ordinates # return numerical x,y for Sensor and Beacon and manhattan distance
 
-	return co_ordinates, grid # return numerical x,y for Sensor and Beacon and manhattan distance
+def part_1(co_ordinates):
+	## Instead of forming grid and filling in - runs nto memory errors because grid is 4.5mill cols and 4.5 mill rows - run loop and calculate all the possible sites for each sensor - and then only record those that are in row 200000. !To Be Done!
+	counter = 0
+	posn = []
+	for line in co_ordinates:
+		sx = line[0][0]
+		sy = line[0][1]
+		bx = line[1][0]
+		by = line[1][1]
+		manhattan = line[2]
+		print(sx,sy,bx,by,manhattan)
+		# loop through all possible points manhattan dist from S
+		for dist_x in range(manhattan+1):
+			dist_y = manhattan-dist_x
+			print(sx+dist_x,sy+dist_y)
+			if sy+dist_y >= 10:
+				if [sx+dist_x, sy+dist_y] not in posn:
+					posn += [sx+dist_x,sy+dist_y]
+					counter += 1
+		print(counter)
+
+
+	return
 
 #input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d15-input.txt')
 input = ['Sensor at x=2, y=18: closest beacon is at x=-2, y=15','Sensor at x=9, y=16: closest beacon is at x=10, y=16','Sensor at x=13, y=2: closest beacon is at x=15, y=3','Sensor at x=12, y=14: closest beacon is at x=10, y=16','Sensor at x=10, y=20: closest beacon is at x=10, y=16','Sensor at x=14, y=17: closest beacon is at x=10, y=16','Sensor at x=8, y=7: closest beacon is at x=2, y=10','Sensor at x=2, y=0: closest beacon is at x=2, y=10','Sensor at x=0, y=11: closest beacon is at x=2, y=10','Sensor at x=20, y=14: closest beacon is at x=25, y=17','Sensor at x=17, y=20: closest beacon is at x=21, y=22','Sensor at x=16, y=7: closest beacon is at x=15, y=3','Sensor at x=14, y=3: closest beacon is at x=15, y=3','Sensor at x=20, y=1: closest beacon is at x=15, y=3']
 
-co_ordinates, grid = parse(input)
+co_ordinates = parse(input)
+part_1(co_ordinates)
 
