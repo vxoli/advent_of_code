946beea29f6c3167260a56ea1da9bb3fc07a39cf
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2022-12-16 13:06:00 +1000

    d15 setup

---
+++ 2022/d15_Beacon_Exclusion_Zone.py
@@ -0,0 +1,13 @@
+# AoC 2022
+# --- Day 15: Beacon Exclusion Zone ---
+
+import urllib.request
+
+def read_url(url):
+	file = urllib.request.urlopen(url)
+	data = file.read().strip()
+	data = data.decode("utf8")
+	data = data.split("\n")
+	
+	return data
+
