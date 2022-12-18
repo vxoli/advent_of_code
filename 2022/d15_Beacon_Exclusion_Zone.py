dc40a92d96ba722374a56d46d05ef9331dd0aa7c
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2022-12-18 19:14:16 +1000

    d15 change to using set over array for coordinates

--- 2022/d15_Beacon_Exclusion_Zone.py
+++ 2022/d15_Beacon_Exclusion_Zone.py
@@ -12,7 +12,7 @@ def read_url(url):
 	return data
 
 def parse(input):
-	co_ordinates = []
+	co_ordinates = set()
 	SENSOR = 'S'
 	BEACON = 'B'
 	max_x = 0
@@ -22,50 +22,44 @@ def parse(input):
 	for line in input:
 		# s = sensor location, b = beacon location
 		s = line.split(':')[0].split(',')
-		s[0] = int(s[0].strip('Sensor at').strip('x='))
-		s[1] = int(s[1].strip().strip('y='))
+		s0 = int(s[0].strip('Sensor at').strip('x='))
+		s1 = int(s[1].strip().strip('y='))
 		b = line.split(':')[1].split(',')
-		b[0] = int(b[0].strip('closest beacon is at').strip('x='))
-		b[1] = int(b[1].strip().strip('y='))
-		manhattan = abs((b[0]-s[0]) + (b[1]-s[1]))
-		co_ordinates.append([s,b,manhattan])
-	max_s_x = max([line[0][0] for line in co_ordinates])
-	max_s_y = max([line[0][1] for line in co_ordinates])
-	max_b_x = max([line[1][0] for line in co_ordinates])
-	max_b_y = max([line[1][1] for line in co_ordinates])
-	min_s_x = min([line[0][0] for line in co_ordinates])
-	min_s_y = min([line[0][1] for line in co_ordinates])
-	min_b_x = min([line[1][0] for line in co_ordinates])
-	min_b_y = min([line[1][1] for line in co_ordinates])
-	offsetx, offsety = min(min_s_x,min_b_x), min(min_s_y,min_b_y)
+		b0 = int(b[0].strip('closest beacon is at').strip('x='))
+		b1 = int(b[1].strip().strip('y='))
+		manhattan = abs(b0-s0) + abs(b1-s1)
+		co_ordinates.add(('S',s0,s1,'B',b0,b1,manhattan))
+
+	# max_s_x = max([line[1] for line in co_ordinates])
+	# max_s_y = max([line[2] for line in co_ordinates])
+	# max_b_x = max([line[4] for line in co_ordinates])
+	# max_b_y = max([line[5] for line in co_ordinates])
+	# min_s_x = min([line[1] for line in co_ordinates])
+	# min_s_y = min([line[2] for line in co_ordinates])
+	# min_b_x = min([line[4] for line in co_ordinates])
+	# min_b_y = min([line[5] for line in co_ordinates])
+	# offsetx, offsety = min(min_s_x,min_b_x), min(min_s_y,min_b_y)
 	
 	return co_ordinates # return numerical x,y for Sensor and Beacon and manhattan distance
 
 def part_1(co_ordinates):
-	## Instead of forming grid and filling in - runs nto memory errors because grid is 4.5mill cols and 4.5 mill rows - run loop and calculate all the possible sites for each sensor - and then only record those that are in row 200000. !To Be Done!
+	## Instead of forming grid and filling in - runs nto memory errors because grid is 4.5mill cols and 4.5 mill rows - run loop and calculate all the possible sites for each sensor - store in set, and then return those that are in row 200000.
 	counter = 0
 	posn = []
 	for line in co_ordinates:
-		sx = line[0][0]
-		sy = line[0][1]
-		bx = line[1][0]
-		by = line[1][1]
-		manhattan = line[2]
+		sx = line[1]
+		sy = line[2]
+		bx = line[4]
+		by = line[5]
+		manhattan = line[6]
 		print(sx,sy,bx,by,manhattan)
-		# loop through all possible points manhattan dist from S
-		for dist_x in range(manhattan+1):
-			dist_y = manhattan-dist_x
-			print(sx+dist_x,sy+dist_y)
-			if sy+dist_y >= 10:
-				if [sx+dist_x, sy+dist_y] not in posn:
-					posn += [sx+dist_x,sy+dist_y]
-					counter += 1
+		# loop through all possible points manhattan dist from S and store coords in no_beacons
 		print(counter)
 
 
 	return
 
-#input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d15-input.txt')
+input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d15-input.txt')
 input = ['Sensor at x=2, y=18: closest beacon is at x=-2, y=15','Sensor at x=9, y=16: closest beacon is at x=10, y=16','Sensor at x=13, y=2: closest beacon is at x=15, y=3','Sensor at x=12, y=14: closest beacon is at x=10, y=16','Sensor at x=10, y=20: closest beacon is at x=10, y=16','Sensor at x=14, y=17: closest beacon is at x=10, y=16','Sensor at x=8, y=7: closest beacon is at x=2, y=10','Sensor at x=2, y=0: closest beacon is at x=2, y=10','Sensor at x=0, y=11: closest beacon is at x=2, y=10','Sensor at x=20, y=14: closest beacon is at x=25, y=17','Sensor at x=17, y=20: closest beacon is at x=21, y=22','Sensor at x=16, y=7: closest beacon is at x=15, y=3','Sensor at x=14, y=3: closest beacon is at x=15, y=3','Sensor at x=20, y=1: closest beacon is at x=15, y=3']
 
 co_ordinates = parse(input)
