21d0e33c3b9afb9b808d07b72e1d7b350b83ebc3
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2022-12-20 22:47:57 +1000

    d20 working out wrap-around

--- 2022/d20_Grove_Positioning_System.py
+++ 2022/d20_Grove_Positioning_System.py
@@ -19,8 +19,39 @@ def read_file(filename): # read from disk
 
 	return data
 
-#input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d20-input.txt')
-#input = [1,2,-3,3,-2,0,4]
-input = read_file("/home/christopher-spectre/Development/advent_of_code/2022/d20-input.txt")
+def part_1(data):
+	# lets work with a dictionary with {key: position in sequence, and value the number}.
+	moves = data.copy()
+	for move in moves:
+		cp = data.index(move)
+		print(data)
+		np = (cp + move)%len(data)
+		list_before = data[:cp]
+		list_after = data[(cp+1)%len(data):]
+		temp = data[cp]
+		print(cp,np,temp)
+		print(list(list_before+[temp]+list_after))
+		num_move = list_after[cp:np]
+		list_remain = list_after[np:]
+		print(num_move, list_remain)
+		print(list(list_before+num_move+[temp]+list_remain))
+		data = list(list_before+num_move+[temp]+list_remain)
 
-for line in input: print(line)
+
+
+	return
+
+# def make_dict(data):
+# 	data_dict = {}
+# 	for i,line in enumerate(input):
+# 		data_dict[i]=line
+# 	return data_dict
+
+#input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d20-input.txt')
+#input = read_file("/home/christopher-spectre/Development/advent_of_code/2022/d20-input.txt")
+input = [1,2,-3,3,-2,0,4]
+part_1(input)
+
+
+
+# move by retrieving number, calulating new position, moving other items up amd storing new position
