8d1a62a193a88f71984ffd40bdfcffdcf1984261
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2022-12-20 19:47:26 +1000

    d20 setup

---
+++ 2022/d20_Grove_Positioning_System.py
@@ -0,0 +1,15 @@
+ AoC 2022
+# --- Day 20: Grove Positioning System ---
+
+import urllib.request
+
+def read_url(url):
+	file = urllib.request.urlopen(url)
+	data = file.read().strip()
+	data = data.decode("utf8")
+	data = data.split("\n")
+	
+	return data
+
+input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d20-input.txt')
+for line in input: print(line)
