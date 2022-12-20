826bc02310e0d172643125cc61f1f3ab4dceedc3
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2022-12-20 20:01:49 +1000

    d20 setup with read from file & url

--- 2022/d20_Grove_Positioning_System.py
+++ 2022/d20_Grove_Positioning_System.py
@@ -3,7 +3,7 @@
 
 import urllib.request
 
-def read_url(url):
+def read_url(url): #read from github file
 	file = urllib.request.urlopen(url)
 	data = file.read().strip()
 	data = data.decode("utf8")
@@ -11,5 +11,16 @@ def read_url(url):
 	
 	return data
 
-input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d20-input.txt')
+def read_file(filename): # read from disk
+	data = []
+	with open(filename) as f:
+		for line in f:
+			data.append(line.strip())
+
+	return data
+
+#input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d20-input.txt')
+#input = [1,2,-3,3,-2,0,4]
+input = read_file("/home/christopher-spectre/Development/advent_of_code/2022/d20-input.txt")
+
 for line in input: print(line)
