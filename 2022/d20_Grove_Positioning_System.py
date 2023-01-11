cac7935517d5de4ac3536c1e0955e2e1d50f3340
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2023-01-12 09:49:20 +1000

    d20 working on test data but error on input data

--- 2022/d20_Grove_Positioning_System.py
+++ 2022/d20_Grove_Positioning_System.py
@@ -38,9 +38,10 @@ def part_1(data):
 
 	return sum
 
-#input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d20-input.txt')
+input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d20-input.txt')
 #input = read_file("/home/christopher-spectre/Development/advent_of_code/2022/d20-input.txt")
-input = [1,2,-3,3,-2,0,4] # Test data
+#input = [1,2,-3,3,-2,0,4] # Test data
+input = [int(i) for i in input]
 print(part_1(input))
 
 
