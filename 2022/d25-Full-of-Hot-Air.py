c9468fdb1356c7ba5ef7861e1169bc956278dd81
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2023-01-12 10:11:27 +1000

    d25 decimal conversion and sum working - thinkinng about reverse conversion

--- 2022/d25-Full-of-Hot-Air.py
+++ 2022/d25-Full-of-Hot-Air.py
@@ -19,6 +19,30 @@ def read_file(filename): # read from disk
 	return data
 
 
-#input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d25-input.txt')
+def convert_snafu_to_decimal(snafu):
+	decimal = 0
+	for i,digit in enumerate(snafu):
+		max_index = len(snafu)-1
+		match digit:
+			case '2': value = 2
+			case '1': value = 1
+			case '-': value = -1
+			case '=': value = -2
+			case '0': value = 0
+		decimal += value * (5 ** (max_index - i))
+
+	return decimal
+
+def convert_decimal_to_snafu(decimal):
+
+	return
+
+input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d25-input.txt')
 #input = read_file("/home/christopher-spectre/Development/advent_of_code/2022/d25-input.txt")
 input = ['1=-0-2','12111','2=0=','21','2=01','111','20012','112','1=-1=','1-12','12','1=','122'] # Test data
+sum = 0
+for digit in input:
+	print(convert_snafu_to_decimal(digit))
+	sum += convert_snafu_to_decimal(digit)
+print(sum)
+print(convert_decimal_to_snafu(sum))
