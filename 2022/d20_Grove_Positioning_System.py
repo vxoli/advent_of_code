0841654934acba4fb0d7d430c6b2030eba2a6903
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2023-01-13 12:24:16 +1000

    d20 working on soln - now working on test input

--- 2022/d20_Grove_Positioning_System.py
+++ 2022/d20_Grove_Positioning_System.py
@@ -19,30 +19,50 @@ def read_file(filename): # read from disk
 
 	return data
 
-def part_1(data):
+def part_1(code):
+	moves = [[int(i),'.'] for i in code]
+	while len([i[1] for i in moves if i[1] == '.']) > 0:
+		posn = 0
+		while moves[posn][1] == 'x': 
+			posn += 1
+		move = moves[posn]
+		num_to_move = move[0]
+		move_from = moves.index([move[0],'.'])
+		if num_to_move < 0: 
+			move_to = (((num_to_move + move_from) % len(moves)) -1) % len(moves)
+			first_part = moves[:move_from]
+			mid_part = moves[move_from+1:move_to+1]
+			end_part = moves[move_to+1:]
+			moves = first_part + mid_part + [[str(num_to_move),'x']] + end_part
 
-	moves = data.copy()
-	for move in moves: 
-	#remove each number from current position (cp) and reinsert into new position (np)
-		if move == 0: continue
-		cp = data.index(move)
-		if move <0: move=move-1
-		if cp+move > len(data): move = move + 1
-		np = (cp+move)%(len(data))
-		temp = data[cp]
-		data= data[:cp]+data[cp+1:]
-		data = data[:np] + [temp] + data[np:]
+		elif num_to_move + move_from > len(moves): 
+			move_to = (((num_to_move + move_from) % len(moves)) +1) % len(moves)
+			first_part = moves[:move_to]
+			mid_part = moves[move_to:move_from]
+			end_part = moves[move_from+1:]
+			moves = first_part + [[str(num_to_move),'x']] + mid_part + end_part
+
+		else:
+			move_to = (num_to_move + move_from) % len(moves)
+			first_part = moves[:move_from]
+			mid_part = moves[move_from+1:move_to+1]
+			end_part = moves[move_to+1:]
+			moves = first_part + mid_part + [[str(num_to_move),'x']] + end_part
+
+	moves = [i[0]  for i in moves]
+	print(moves)
 	sum = 0
+	print(moves.index('0'))
 	for i in (1000,2000,3000):
-		sum += data[(data.index(0)+(i%len(data)))%len(data)]
+		print(int((moves[(i+moves.index('0'))%(len(moves))])))
+		sum += int((moves[(i+moves.index('0'))%(len(moves))]))
 
 	return sum
 
-input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d20-input.txt')
+#input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d20-input.txt')
 #input = read_file("/home/christopher-spectre/Development/advent_of_code/2022/d20-input.txt")
-#input = [1,2,-3,3,-2,0,4] # Test data
-input = [int(i) for i in input]
-print(part_1(input))
+input = ['1','2','-3','3','-2','0','4'] # Test data
+print("What is the sum of the three numbers that form the grove coordinates?",part_1(input))
 
 
 
