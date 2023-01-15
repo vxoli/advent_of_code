6e5fd9bff16c2fe16478e210fa694732c8ec3490
Christopher STONELL <vxoli-dfoxb@yahoo.co.uk>
2023-01-15 13:27:28 +1000

    d20 improved part 1 algorithm

--- 2022/d20_Grove_Positioning_System.py
+++ 2022/d20_Grove_Positioning_System.py
@@ -6,7 +6,7 @@ import urllib.request
 def read_url(url): #read from github file
 	file = urllib.request.urlopen(url)
 	data = file.read().strip()
-	data = data.decode("utf8")
+	data = data.detemp("utf8")
 	data = data.split("\n")
 	return data
 
@@ -21,37 +21,60 @@ def swap(nums, a, b):
 	nums[a],nums[b]=nums[b],nums[a]
 	return nums
 
-def part_1(code):
-	moves = code.copy()
-	moves = [[int(i),'.'] for i in moves]
-	n = len(moves)
-	while len([i[1] for i in moves if i[1] == '.']) > 0:
-		posn = 0
-		while moves[posn][1] == 'x':
-			posn += 1
-		move = moves[posn]
-		num_to_move = move[0]
-#		if num_to_move == 0: continue
-		move_from = moves.index([move[0],'.'])
-		moves[posn] = [num_to_move,'x']
-		move_to = (move_from + num_to_move) % n
-		difference = (move_to - move_from)%n
-		if num_to_move < 0: difference = (difference + n -1) % n
+def part_1(moves):
+	code = moves.copy()
+#	code  = [[int(i),'.'] for i in code]
+	n = len(code)
+	for move in moves:
+		posn = code.index(move)
+		move_to = (posn + move) % n
+		difference = (move_to - posn) % n
+		if move < 0: difference = (difference + n -1) % n
+		# print(code)
+		# print(move)
+		# print(posn, move_to, difference)
 		for i in range(difference):
-			moves = swap(moves,(i+posn)%n,(i+posn+1)%n)
-		if move_to-move_from<0:
-			moves = [moves[n-1]] + moves[:n-1]
-
-	index = moves.index([0,'x'])
-	print(moves[index])
+			code = swap(code,(i+posn)%n,(i+posn+1)%n)
+		if move_to-posn<0 and move > 0:
+			code = [code[n-1]] + code[:n-1]
+	index = code.index(0)
 	sum = 0
-	for i in (1001,2001,3001):
-		print(int((moves[((i+index)%(len(moves)))][0])))
-		sum += int((moves[((i+index)%(len(moves)))][0]))
+	for i in (1000,2000,3000):
+		sum += int((code[((i+index)%(len(code)))]))
 
 	return sum
 
-input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d20-input.txt')
+def part_2(moves):
+	code = moves.copy()
+#	code  = [[int(i),'.'] for i in code]
+	n = len(code)
+	for move in moves:
+		posn = code.index(move)
+		move_to = (posn + move) % n
+		difference = (move_to - posn) % n
+		if move < 0: difference = (difference + n -1) % n
+		# print(code)
+		# print(move)
+		# print(posn, move_to, difference)
+		for i in range(difference):
+			code = swap(code,(i+posn)%n,(i+posn+1)%n)
+		if move_to-posn<0 and move > 0:
+			code = [code[n-1]] + code[:n-1]
+	index = code.index(0)
+	sum = 0
+	for i in (1000,2000,3000):
+		sum += int((code[((i+index)%(len(code)))]))
+
+
+		# print(code)
+
+
+	return sum
+
+
+#input = read_url('https://raw.githubusercontent.com/vxoli/adventofcode/main/2022/d20-input.txt')
 #input = read_file("/home/christopher-spectre/Development/advent_of_code/2022/d20-input.txt")
-#input = ['1','2','-3','3','-2','0','4'] # Test data
-print("What is the sum of the three numbers that form the grove coordinates?",part_1(input))
+input = ['1','2','-3','3','-2','0','4'] # Test data
+print("Part 1: What is the sum of the three numbers that form the grove coordinates?",part_1([int(i) for i in input]))
+
+print(part_2([int(i) for i in input]))
