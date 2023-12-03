c6ffaa6200976563bee0d11a3d70037a1b1ef95f
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-03 20:12:33 +1000

    added d2 and d3

--- 2023/d01_Trebuchet.py
+++ 2023/d01_Trebuchet.py
@@ -17,33 +17,45 @@ def read_data(filename):
 
 	return lines
 
-def text2int(textnum, numwords={}):
-    if not numwords:
-      units = [
-        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
-        "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-        "sixteen", "seventeen", "eighteen", "nineteen",
-      ]
-
-      tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-
-      scales = ["hundred", "thousand", "million", "billion", "trillion"]
-
-      numwords["and"] = (1, 0)
-      for idx, word in enumerate(units):    numwords[word] = (1, idx)
-      for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
-      for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
-
-    current = 0
-    result = ""
-    for word in textnum.split():
-        if word not in numwords:
-          increment = word
-        else:
-            scale, increment = numwords[word]
-        result = result + str(increment)
-        
-    return result
+def text2int(string):
+  numberWords = {
+    'zero' : '0',
+    'one' : '1',
+    'two' : '2',
+    'three' : '3',
+    'four' : '4',
+    'five' : '5',
+    'six' : '6',
+    'seven' : '7',
+    'eight' : '8',
+    'nine' : '9',
+    'ten' : '10',
+    'eleven' : '11',
+    'twelve' : '12',
+    'thirteen' : '13',
+    'fourteen' : '14',
+    'fifteen' : '15',
+    'sixteen' : '16',
+    'seventeen' : '17',
+    'eighteen' : '18',
+    'nineteen' : '19',
+    'twenty' : '20'
+  }
+  result = ""
+  currentWord = ""
+  for char in string:
+    if char.isalpha():
+      currentWord += char
+      for s in list(numberWords.keys()):
+        if s in currentWord:
+          result += numberWords.get(s.lower(), currentWord)
+          currentWord = ""
+    else:
+      result += char
+      currentWord = ""
+    print(result)
+  print(result)
+  return str(result)
 
 # Part 1
 
@@ -56,14 +68,17 @@ for line in calibrationDocument:
 print("Part 1: Consider your entire calibration document. What is the sum of all of the calibration values?", sum)
 
 # Part 2
-numberWords = ["one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+calibrationDocument = ['two1nine',
+'eightwothree',
+'abcone2threexyz',
+'xtwone3four',
+'4nineeightseven2',
+'zoneight234',
+'7pqrstsixteen']
 sum = 0
 for line in calibrationDocument:
     number = ""
-    for numberWord in numberWords:
-        if numberWord in line:
-            number = number + " " + numberWord
-    num = text2int(number)
-    print(num,num[0],num[-1], num[0]+num[-1])
-    sum += int(num[0]+num[-1])
+    number = text2int(line)
+    print(number,"|",number[0],"-",number[-1],"=", number[0]+number[-1])
+    sum += int(number[0]+number[-1])
 print(sum)
