e672d6966d9ddb63c15cbe0aee61c39851d1b881
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-05 08:30:43 +1000

    d02 part 2 working! Correct solution

--- 2023/d02_Cube_Conundrum.py
+++ 2023/d02_Cube_Conundrum.py
@@ -35,6 +35,11 @@ for line in data:
 print("What is the sum of the power of these sets?",sum)                
 
 # PART 2
+""" data = ['Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'] """
 sum = 0
 for line in data:
     print(line)
@@ -51,6 +56,8 @@ for line in data:
         max_red = max(red_cubes, max_red)
         max_blue = max(blue_cubes, max_blue)
         max_green = max(green_cubes, max_green)
-        power = max_red * max_blue * max_green
-        sum += power
+    print(max_red,max_green,max_blue)
+    power = max_red * max_blue * max_green
+    sum += power
+    print(power, sum)
 print(sum)        
