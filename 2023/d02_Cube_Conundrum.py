163194b513941eaf7ead2994e658c19889408946
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-04 22:21:51 +1000

    working on part 2 - logic in place but an error...

--- 2023/d02_Cube_Conundrum.py
+++ 2023/d02_Cube_Conundrum.py
@@ -33,3 +33,24 @@ for line in data:
     sum += int(game_number) * possible
 
 print("What is the sum of the power of these sets?",sum)                
+
+# PART 2
+sum = 0
+for line in data:
+    print(line)
+    max_red = max_blue = max_green = 0
+    possible = True
+    game_number = line.split(":")[0].split(" ")[1]
+    game_draw = line.split(":")[1].split(";")
+    for play in game_draw:
+        cubes = play.split(",")
+        print(cubes)
+        red_cubes = int(max([x.split(" ")[1] if x.split(" ")[2] == "red" else "0" for x in cubes]))
+        blue_cubes = int(max([x.split(" ")[1] if x.split(" ")[2] == "blue" else "0" for x in cubes]))
+        green_cubes = int(max([x.split(" ")[1] if x.split(" ")[2] == "green" else "0" for x in cubes]))
+        max_red = max(red_cubes, max_red)
+        max_blue = max(blue_cubes, max_blue)
+        max_green = max(green_cubes, max_green)
+        power = max_red * max_blue * max_green
+        sum += power
+print(sum)        
