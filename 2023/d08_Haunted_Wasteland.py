729d95345451ed8b9616223d33916ac3754fcba5
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-11 08:16:56 +1000

    d08 data cleaning

--- 2023/d08_Haunted_Wasteland.py
+++ 2023/d08_Haunted_Wasteland.py
@@ -17,3 +17,10 @@ def read_data(filename):
 
 ## MAIN
 data = read_url("https://raw.githubusercontent.com/vxoli/advent_of_code/main/2023/d08_input.txt")
+data = ['RL','','AAA = (BBB, CCC)', 'BBB = (DDD, EEE)','CCC = (ZZZ, GGG)','DDD = (DDD, DDD)','EEE = (EEE, EEE)','GGG = (GGG, GGG)','ZZZ = (ZZZ, ZZZ)']
+moves = data [0]
+data = data[2:-1]
+map = dict()
+map = dict([(x.split(' = ')[0], x.split(' = ')[1]) for x in data])
+move = 0
+locationList = [x.split(' = ')[0] for x in data]
