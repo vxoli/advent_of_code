94ecfd63c502c2b9acd26a43f0e2d72b24157107
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-11 17:09:37 +1000

    d07 p1 works on test data

--- 2023/d09_Mirage_Maintenance.py
+++ 2023/d09_Mirage_Maintenance.py
@@ -20,13 +20,21 @@ data = read_url("https://raw.githubusercontent.com/vxoli/advent_of_code/main/202
 
 # PART 1
 data = [[0, 3, 6, 9, 12, 15,],[1, 3, 6, 10, 15, 21],[10, 13, 16, 21, 30, 45]]
-
+total = 0
 for line in data:
     index = 0
     differences = []
+    lastDiff = 0
     differences.append(line)
     while sum(differences[(index)*(index>0)]) > 0:
-        print(differences[(index)*(index>0)], sum(differences[(index)*(index>0)]))
         differences.append([differences[index][i+1]-differences[index][i] for i in range(len(differences[index])-1)])
-        print(differences)
+        lastDiff = differences[index][-1]
         index += 1
+    predictedNext = 0
+    newDiff = lastDiff
+    for i in reversed(differences[0:len(differences)-1]):
+        predictedNext = newDiff + i[-1]
+        newDiff = predictedNext
+    print(predictedNext-lastDiff)
+    total += predictedNext-lastDiff
+print(total)
