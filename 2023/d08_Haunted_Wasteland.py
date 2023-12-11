9473843411d012914ff3d4086cc487b38ac44046
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-11 14:30:12 +1000

    d07 p1 correct!

--- 2023/d08_Haunted_Wasteland.py
+++ 2023/d08_Haunted_Wasteland.py
@@ -17,8 +17,8 @@ def read_data(filename):
 
 ## MAIN
 data = read_url("https://raw.githubusercontent.com/vxoli/advent_of_code/main/2023/d08_input.txt")
-data = ['RL','','AAA = (BBB, CCC)', 'BBB = (DDD, EEE)','CCC = (ZZZ, GGG)','DDD = (DDD, DDD)','EEE = (EEE, EEE)','GGG = (GGG, GGG)','ZZZ = (ZZZ, ZZZ)']
-data = ['LLR', '', 'AAA = (BBB, BBB)', 'BBB = (AAA, ZZZ)', 'ZZZ = (ZZZ, ZZZ)']
+# data = ['RL','','AAA = (BBB, CCC)', 'BBB = (DDD, EEE)','CCC = (ZZZ, GGG)','DDD = (DDD, DDD)','EEE = (EEE, EEE)','GGG = (GGG, GGG)','ZZZ = (ZZZ, ZZZ)']
+# data = ['LLR', '', 'AAA = (BBB, BBB)', 'BBB = (AAA, ZZZ)', 'ZZZ = (ZZZ, ZZZ)']
 moves = data [0]
 data = data[2:-1]
 map = dict([(x.split(' = ')[0], x.split(' = ')[1]) for x in data])
@@ -30,4 +30,4 @@ while location != 'ZZZ':
     index = (index+1) * (index+1 < len(moves))
     location = map[location].strip('(').strip(')').split(', ')[0 + (move=='R')]
     steps += 1
-print(steps)
+print("Part 1: Starting at AAA, follow the left/right instructions. How many steps are required to reach ZZZ?",steps)
