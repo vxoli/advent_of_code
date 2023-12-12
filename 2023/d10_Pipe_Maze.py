b5bafe66bc25c2eba17215585cab7559044f1c63
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-12 16:22:07 +1000

    d10 p1 finding S and setting constants git push

--- 2023/d10_Pipe_Maze.py
+++ 2023/d10_Pipe_Maze.py
@@ -17,3 +17,19 @@ def read_data(filename):
 
 ## MAIN
 data = read_url("https://raw.githubusercontent.com/vxoli/advent_of_code/main/2023/d10_input.txt")
+data = ['.....','.S-7.','.|.|.','.L-J.','.....']
+# define movements in a dict (pipe : (dx,dy))
+pipes = dict([('|',(0,1)),('-',(1,0)),('L',(1,0)),('J',(-1,0)),('7',()),('F',()),('.',()),('S',())])
+directions = dict([('U',(0,-1)), ('UR', (-1,1)), ('R', (1,0)), ('DR', (1,1)), ('D', (0,1)), ('DL', (1,-1)), ('L', (0,-1)), ('UL',(-1,-1))])
+# find the starting point: store co-ords in list
+for row, line in enumerate(data):
+    if 'S' in line:
+        rowS = row
+        colS = line.index('S')
+# scan around clockwise and find next possible connected pipe
+# move to next point based - store co-ordinates etc...
+for direction in directions.keys():
+    print(directions[direction])
+
+
+# once back to S count number of moves and divide by 2.
