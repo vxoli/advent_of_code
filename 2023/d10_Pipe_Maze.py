9f2e1c78782d81d8879d5ee246354641d26853fd
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-15 15:37:28 +1000

    d10 p1 following path seems to work on test data

--- 2023/d10_Pipe_Maze.py
+++ 2023/d10_Pipe_Maze.py
@@ -49,8 +49,8 @@ def startMove(row, col, pipe):
         if direction == 'D' and nextPipe in ['|','J','L']:
             match nextPipe:
                 case '|':
-                    row = row
-                    col += 1
+                    row += 1
+                    col = col
                     pipe = nextPipe
                 case 'J':
                     row -= 1
@@ -70,11 +70,12 @@ def startMove(row, col, pipe):
             break
     pipe = nextPipe
     print(row, col, pipe)
-    return(row, col, pipe)
+    return(row, col, pipe, direction)
 
 ## MAIN
 data = read_url("https://raw.githubusercontent.com/vxoli/advent_of_code/main/2023/d10_input.txt")
 data = ['.....','.S-7.','.|.|.','.L-J.','.....']
+data = ['7-F7-','.FJ|7','SJLL7','|F--J','LJ.LJ']
 # define movements in a dict (pipe : (dx,dy))
 pipes = dict([('|',(0,1)),('-',(1,0)),('L',(1,0)),('J',(-1,0)),('7',()),('F',()),('.',()),('S',())])
 directions = dict([('U',(-1,0)), ('R', (0,1)), ('D', (1,0)), ('L', (0,-1))])
@@ -84,40 +85,77 @@ for row, line in enumerate(data):
         rowS = row
         colS = line.index('S')
 pipe = [('S',(rowS,colS))]
+print(pipe)
 nextPipe = ""
 # scan around clockwise and find next possible connected pipe
 # move to next point based - store co-ordinates etc...
 row = rowS
 col = colS
-i=0
+i=1
 # look for valid 1st pipe off starting point
-row,col,pipe = startMove(rowS, colS, 'S')
-
+row,col,pipe, lastMove = startMove(rowS, colS, 'S')
+print(row,col)
 # now follow the route
-while i < 10:
+while (pipe != 'S') and (i < 16):
+
     match pipe:
         case '-':
-            row =row
-            col += 1
+            if lastMove == 'R':
+                row = row
+                col += 1
+            if lastMove == 'L':
+                row = row
+                col -= 1
         case '|':
-            row += 1
-            col = col
+            if lastMove == 'D':
+                row += 1
+                col = col
+            if lastMove == 'U':
+                row -= 1
+                col = col
         case 'L':
-            row += 1
-            col += 1
+            if lastMove == 'D':
+                row = row
+                col += 1
+                lastMove = 'R'
+            if lastMove == 'L':
+                row -= 1
+                col = col
+                lastMove = 'U'            
         case 'J':
-            row = row
-            col -= 1
+            if lastMove == 'D':
+                row = row
+                col -= 1
+                lastMove = 'L'
+            if lastMove == 'R':
+                row -= 1
+                col = col
+                lastMove = 'U'
         case '7':
-            row += 1
-            col = col
+            if lastMove == 'R':
+                row += 1
+                col = col
+                lastMove = 'D'
+            if lastMove == 'U':
+                row = row
+                col -= 1
+                lastMove = 'L'
         case 'F':
-            row -= 1
-            col += 1
+            if lastMove == 'U':
+                row = row
+                col += 1
+                lastMove = 'R'
+            if lastMove == 'L':
+                row += 1
+                col = col
+                lastMove = 'D'
     pipe = data[row][col]
+    
     print(row,col,pipe,i)
     i += 1
 
         
             
 # once back to S count number of moves and divide by 2.
+print(row,col,pipe,i)
+print("Part 1: How many steps along the loop does it take to get from the starting position to the point farthest from the starting position?", int(i/2))
