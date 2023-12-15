4a6dccae2e5b6f051ea5e183629ce97beb430333
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-15 16:05:02 +1000

    d11 part1 started

--- 2023/d11_Cosmic_Expansion.py
+++ 2023/d11_Cosmic_Expansion.py
@@ -17,3 +17,22 @@ def read_data(filename):
 
 # MAIN
 data = read_url("https://raw.githubusercontent.com/vxoli/advent_of_code/main/2023/d11_input.txt")
+data = ['...#......',
+'.......#..',
+'#.........',
+'..........',
+'......#...',
+'.#........',
+'.........#',
+'..........',
+'.......#..',
+'#...#.....']
+# look for rows with no galaxies and double
+universe = []
+for i,row in enumerate(data):
+    universe.append(row)
+    if '#' not in row:
+        print("Row",i,"No galaxy")
+        universe.append(row)
+print(universe)    
+#then look for columsn with no galaxies and double them
