3d0d28d2dfbae38a2e6d82dfef27aed4e7d515e8
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-16 14:08:50 +1000

    d15 p1 working on test data

--- 2023/d15_Lens_Library.py
+++ 2023/d15_Lens_Library.py
@@ -16,4 +16,21 @@ def read_data(filename):
     return lines
 
 # MAIN
-data = read_url("https://raw.githubusercontent.com/vxoli/advent_of_code/main/2023/d15_input.txt")
+sequence = read_url("https://raw.githubusercontent.com/vxoli/advent_of_code/main/2023/d15_input.txt")
+sequence = ['rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7']
+# sequence = ['HASH']
+sequence = sequence[0].split(',')
+sum = 0
+for step in sequence:
+    value = 0
+    for char in step:
+        value += ord(char)
+        value *= 17
+        value = value % 256
+        print(value)
+    sum += value
+        
+        
+
+
+print("Part 1: What is the sum of the results?", sum)
