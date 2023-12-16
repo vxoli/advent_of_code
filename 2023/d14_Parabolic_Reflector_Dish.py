c17502b6f315b9e7a09f3b4255897ce491269adf
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-16 13:41:23 +1000

    d14 p1 working on test data

--- 2023/d14_Parabolic_Reflector_Dish.py
+++ 2023/d14_Parabolic_Reflector_Dish.py
@@ -18,7 +18,7 @@ def read_data(filename):
 # MAIN
 data = read_url("https://raw.githubusercontent.com/vxoli/advent_of_code/main/2023/d14_input.txt")
 data =['O....#....',
-'O.OO#....#,'
+'O.OO#....#',
 '.....##...',
 'OO.#O....O',
 '.O.....O#.',
@@ -32,3 +32,21 @@ numRows = len(data)
 numCols = len(data[0])
 map = dict()
 map = {(i, j): char for i, row in enumerate(data) for j, char in enumerate(row)}
+# start from north edge and move south
+# if find a O rock see if can roll north to a '.'. If north place contatins 'O' or '#' can roll.
+# if rolls replace with empty '.'
+for i in range (numRows-2):
+    for y in range (1,numRows):
+        for x in range(numCols):
+            if map[(y,x)] == "O" and map[(y-1,x)] ==".":
+                map[(y-1,x)] = "O"
+                map[(y,x)] = '.'
+# assign the loading to the rows and sum
+loadTotal = 0
+for y in range(numRows):
+    rowOcount = 0
+    for x in range(numCols):
+        if map[(y,x)] == "O":
+            rowOcount += 1
+    loadTotal += rowOcount * (numRows-y)
+print("Part 1: what is the total load on the north support beams?", loadTotal)
