ee8f7f4fffeaf83c23dd3fcd98d794c9415fad5c
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-16 12:03:12 +1000

    d12 p1 data wrangling

--- 2023/d14_Parabolic_Reflector_Dish.py
+++ 2023/d14_Parabolic_Reflector_Dish.py
@@ -17,3 +17,18 @@ def read_data(filename):
 
 # MAIN
 data = read_url("https://raw.githubusercontent.com/vxoli/advent_of_code/main/2023/d14_input.txt")
+data =['O....#....',
+'O.OO#....#,'
+'.....##...',
+'OO.#O....O',
+'.O.....O#.',
+'O.#..O.#.#',
+'..O..#O..O',
+'.......O..',
+'#....###..',
+'#OO..#....']
+# store map in dict of co-ordinates key = (x,y) value = char
+numRows = len(data)
+numCols = len(data[0])
+map = dict()
+map = {(i, j): char for i, row in enumerate(data) for j, char in enumerate(row)}
