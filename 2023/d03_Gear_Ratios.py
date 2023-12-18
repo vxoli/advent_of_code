9fa02b42c5d9a2238526665329552ce5d2bb370b
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-18 12:37:53 +1000

    d03 working on test data set

--- 2023/d03_Gear_Ratios.py
+++ 2023/d03_Gear_Ratios.py
@@ -29,19 +29,35 @@ data = ['467..114..',
 '.664.598..']
 
 # PART 1
-# search across each row - if a number found then look for symbols above or below or next to it.
-# if no numbers - ignore
-# if there are numbers add the number to the total
+# search across each row - if a number found then add to list with co-ordinates
 
-print(data)
 numbers = []
 for y,row in enumerate(data):
     numStr = ""
     for x,char in enumerate(row):
         if char in ['0','1','2','3','4','5','6','7','8','9']:
             numStr += char
-        if char in ['.','!','@','#','$','%','^','&','*','(',')'] and numStr != "":
-            numbers.append(numStr)
+        if char in ['.','!','@','#','$','%','^','&','*','(',')','+','='] and numStr != "":
+            numbers.append((numStr,(x-len(numStr),y)))
             numStr = ""
  
-    print(numbers)
+# now look for symbols above or below or next to the numbers.
+# if there are symbols add the number to the total
+total = 0
+for value in numbers:
+    number = value[0]
+    coords = value[1]
+    xStart = coords[0]
+    xEnd = xStart+len(number)-1
+    y = coords[1]
+    xStart = ((xStart -1)>=0) * (xStart-1) + ((xStart -1) <0) * 0
+    xEnd = ((xEnd +1) > (len(data[0])-1) * (xEnd)) + (((xEnd +1) <= len(data[0])-1) * (xEnd+1))
+    yStart = ((y-1)>=0)*(y-1) + ((y-1)<0)*0 
+    yEnd = ((y+1)<=len(data)-1)*(y+1) + ((y+1)>len(data)-1)*y
+    for x in range(xStart,xEnd+1):
+        for y in range(yStart,yEnd+1):
+            addNumber = 0
+            if data[y][x] in ['!','@','#','$','%','^','&','*','(',')','+','=']:
+                addNumber=1
+            total += int(number)*addNumber
+print("Part 1: What is the sum of all of the part numbers in the engine schematic?",total)
