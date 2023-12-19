714da35d1828a833e325ca05e8c3cf58a6f1bb65
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-19 11:35:09 +1000

    d16 files added

---
+++ 2023/d16_The_Floor_Will_Be_Lava.py
@@ -0,0 +1,19 @@
+# --- Day 16: The Floor Will Be Lava ---
+
+import urllib.request
+
+def read_url(url):
+    file = urllib.request.urlopen(url)
+    data = file.read().strip()
+    data = data.decode("utf8")
+    data = data.split("\n")
+    return data
+
+def read_data(filename):
+    with open(filename) as file:
+        lines = list(map(str, file.readlines()))
+    file.close()
+    return lines
+
+# MAIN
+data = read_url("https://raw.githubusercontent.com/vxoli/advent_of_code/main/2023/d16_input.txt")
