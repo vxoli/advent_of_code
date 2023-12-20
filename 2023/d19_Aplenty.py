97ce25bfceb291dbc9abef7c9c868f124332e88c
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-20 19:03:32 +1000

    d19 fils setup

---
+++ 2023/d19_Aplenty.py
@@ -0,0 +1,19 @@
+# --- Day 19: Aplenty ---
+
+import urllib.request
+
+def read_url(url):
+    file = urllib.request.urlopen(url)
+    data = file.read().strip()
+    data = data.decode("utf8")
+    data = data.split("\n")
+    return data
+
+def read_data(filename):
+    with open(filename) as file:
+        lines = list(map(str, file.readlines()))
+    file.close()
+    return lines
+
+# MAIN
+data = read_url("https://raw.githubusercontent.com/vxoli/advent_of_code/main/2023/d19_input.txt")
