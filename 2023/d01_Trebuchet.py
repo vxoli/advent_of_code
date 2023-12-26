43f9cb19851a100d652c2825a380732132b035ed
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-26 13:07:14 +1000

    d01 part 2 working on test data

--- 2023/d01_Trebuchet.py
+++ 2023/d01_Trebuchet.py
@@ -18,48 +18,29 @@ def read_data(filename):
 	return lines
 
 def text2int(string):
-  numberWords = {
-    'zero' : '0',
-    'one' : '1',
-    'two' : '2',
-    'three' : '3',
-    'four' : '4',
-    'five' : '5',
-    'six' : '6',
-    'seven' : '7',
-    'eight' : '8',
-    'nine' : '9',
-    'ten' : '10',
-    'eleven' : '11',
-    'twelve' : '12',
-    'thirteen' : '13',
-    'fourteen' : '14',
-    'fifteen' : '15',
-    'sixteen' : '16',
-    'seventeen' : '17',
-    'eighteen' : '18',
-    'nineteen' : '19',
-    'twenty' : '20'
-  }
-  result = ""
-  currentWord = ""
-  for char in string:
-    if char.isalpha():
-      currentWord += char
-      for s in list(numberWords.keys()):
-        if s in currentWord:
-          result += numberWords.get(s.lower())
-          currentWord = ""
-    else:
-      result += char
-      currentWord = ""
-    print(result)
-  print(result)
-  return str(result)
+
+  for word, number in numberWords.items():
+    string = string.replace(str(number), word)
+  return string
+    
+def find_substrings_with_digits(string):
+  substrings_with_indicies = []
+  for word in numberWords:
+    index = string.find(word)
+    while index != -1:
+      substrings_with_indicies.append((word, index))
+      index = string.find(word, index+1)
+  substrings_with_indicies.sort(key=lambda x:x[1])
+  substrings_translated = [numberWords[word] for word,_ in substrings_with_indicies]
+  return substrings_translated
 
 # Part 1
 
 calibrationDocument = read_url("https://raw.githubusercontent.com/vxoli/advent_of_code/main/2023/d01_input.txt")
+calibrationDocument = ['1abc2',
+'pqr3stu8vwx',
+'a1b2c3d4e5f',
+'treb7uchet']
 sum = 0
 for line in calibrationDocument:
     digits = ''.join(c for c in line if c.isdigit())
@@ -68,17 +49,36 @@ for line in calibrationDocument:
 print("Part 1: Consider your entire calibration document. What is the sum of all of the calibration values?", sum)
 
 # Part 2
-""" calibrationDocument = ['two1nine',
+# tried converting words to numbers - didn't work - too complicated.
+# now trying converting numbers to words then back again...
+# need to accont for lines with only single digit.
+numberWords = {
+  'zero' : 0,
+  'one' : 1,
+  'two' : 2,
+  'three' : 3,
+  'four' : 4,
+  'five' : 5,
+  'six' : 6,
+  'seven' : 7,
+  'eight' : 8,
+  'nine' : 9
+}
+calibrationDocument = ['two1nine',
 'eightwothree',
 'abcone2threexyz',
 'xtwone3four',
 '4nineeightseven2',
 'zoneight234',
 '7pqrstsixteen']
- """
 sum = 0
 for line in calibrationDocument:
-    number = text2int(line)
-    print(number,"|",number[0],"-",number[-1],"=", number[0]+number[-1])
-    sum += int(number[0]+number[-1])
+  numbersInWords = text2int(line)
+  substrings = find_substrings_with_digits(numbersInWords)
+  first = substrings[0]
+  last = substrings[-1]
+  final_digit = first*10 + last
+  print(final_digit)
+  sum += final_digit
+  
 print("Part 2: What is the sum of all of the calibration values?", sum)
