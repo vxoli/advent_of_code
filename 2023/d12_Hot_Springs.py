bedf23ab525beb8ebe26d0bb9409f461d35fc128
vxoli <vxoli-dfoxb@yahoo.co.uk>
2023-12-28 11:07:50 +1000

    d12 part 1 working on test data

--- 2023/d12_Hot_Springs.py
+++ 2023/d12_Hot_Springs.py
@@ -15,5 +15,53 @@ def read_data(filename):
     file.close()
     return lines
 
+def arrangements(conditions, groups, memo={}):
+    if len(conditions) == 0:
+        if len(groups) == 0:
+            return 1
+        else:
+            return 0
+ 
+    if len(groups) == 0:
+        if "#" in conditions:
+            return 0
+        else:
+            return 1
+    if (conditions, groups) in memo:
+        return memo[(conditions, groups)]
+    curr, size = conditions[0], groups[0]
+    counts = 0
+    if curr == "." or curr == "?":
+        counts += arrangements(conditions[1:], groups, memo)
+    if (
+        (curr == "#" or curr == "?")
+        and len(conditions) >= size
+        and "." not in conditions[:size]
+        and (size == len(conditions) or conditions[size] != "#")
+    ):
+        counts += arrangements(conditions[size + 1 :], groups[1:])
+    memo[(conditions, groups)] = counts
+    return counts
+
+
+    """ if curr ((curr == "#" or curr =="?")
+             and len(conditions) >= size
+             and "." not in conditions[:size]
+             and (size == len(conditions) or conditions[size] != "#")):
+        counts += arrangements(conditions[size+1:], groups[1:])
+     """
 # MAIN
-data = read_url("https://raw.githubusercontent.com/vxoli/advent_of_code/main/2023/d12_input.txt")
+map = read_url("https://raw.githubusercontent.com/vxoli/advent_of_code/main/2023/d12_input.txt")
+map = ['???.### 1,1,3',
+'.??..??...?##. 1,1,3',
+'?#?#?#?#?#?#?#? 1,3,1,6',
+'????.#...#... 4,1,1',
+'????.######..#####. 1,6,5',
+'?###???????? 3,2,1']
+lines = []
+answer = 0
+for line in map:
+    conditions, groups = line.split(" ")
+    groups = tuple([int(x) for x in groups.split(",")])
+    answer += arrangements(conditions, groups)
+print(answer)
